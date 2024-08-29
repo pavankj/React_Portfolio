9f013c4626cba01120c3e5cef1aa1b6a8b8bb037
pavankj <pavankj22121997@gmail.com>
2024-08-29 03:26:36 -0500

    Changes in Contact.jsx file

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import styles from "./contact.module.css"
+import styles from "./Contact.module.css"
 import { getImageUrl } from '../../utils'
 
 const Contact = () => {
